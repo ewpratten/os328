758faa9a300e8d6cdeefb3164b6fa484a910e3a9
Evan Pratten <ewpratten@gmail.com>
2019-02-22 21:01:35 -0500

    updated header

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "include/io/serial.h"
 
 int main(){
+    // Start serial connection and redirect stdin/out
     uart_init();
     stdout = &uart_output;
     stdin  = &uart_input;
 
-    puts("Hello world!");
+    puts("AVR-X");
+    puts("By: Evan Pratten <ewpratten@gmail.com>\n");
+    puts("Starting");
 
     return 0;
 }
